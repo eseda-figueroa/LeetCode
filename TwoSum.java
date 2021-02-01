3404068036f12abf97b90584bdc1789bae6542c7
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2021-01-31 22:28:17 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -28,8 +28,8 @@ class Solution
     public int[] twoSum(int[] nums, int target) 
     {
         int[] sum = new int[2];
-        for(int i = 0; i < nums.length && nums[i] < target; i++)
-            for(int j = i+1; j < nums.length && nums[j] < target; j++)
+        for(int i = 0; i < nums.length && nums[i] <= target; i++) 
+            for(int j = i+1; j < nums.length && nums[j] <= target; j++)
             {
                 if(nums[i] + nums[j] == target)
                 {
