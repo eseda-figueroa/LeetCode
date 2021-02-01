ca387010a655e088018a9cc7249f423ec4231d73
Elian Seda-Figueroa <73006480+eseda-figueroa@users.noreply.github.com>
2021-01-31 22:20:41 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -8,36 +8,37 @@
 class Solution 
 {
     // O(n^2) brute force approach
-    // public int[] twoSum(int[] nums, int target) 
-    // {
-    //     int[] sum = new int[2];
-    //     for(int i = 0; i < nums.size(); i++)
-    //         for(int j = i+1; j < nums.size(); j++)
-    //         {
-    //             if(nums[i] + nums[j] == target)
-    //             {
-    //                 sum[0] = i;
-    //                 sum[1] = j;
-    //                 return sum;
-    //             }     
-    //         }
+    //  public int[] twoSum(int[] nums, int target) 
+    //  {
+    //      int[] sum = new int[2];
+    //      for(int i = 0; i < nums.length; i++)
+    //          for(int j = i+1; j < nums.length; j++)
+    //          {
+    //              if(nums[i] + nums[j] == target)
+    //              {
+    //                  sum[0] = i;
+    //                  sum[1] = j;
+    //                  return sum;
+    //              }     
+    //          }
+    //     return sum;
     // }
     
     // faster but still O(n^2) worst case
     public int[] twoSum(int[] nums, int target) 
     {
         int[] sum = new int[2];
-        for(int i = 0; i < nums.size() && nums[i] < target; i++)
-            for(int j = i+1; j < nums.size() && nums[j] < target; j++)
+        for(int i = 0; i < nums.length && nums[i] < target; i++)
+            for(int j = i+1; j < nums.length && nums[j] < target; j++)
             {
                 if(nums[i] + nums[j] == target)
                 {
                     sum[0] = i;
                     sum[1] = j;
                     return sum;
-                }
-                    
-            }      
+                }      
+            }
+        return sum;
      }
     
 }
